05a51a8a1aecb0cce16f2b31a2251210b7cabfa9
Abokina Wilson-Jones <abo_jones@hotmail.co.uk>
2020-02-28 19:21:13 +0000

    Complete app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loan: 0,
+      years: 1,
+      maxYears: 12,
+      payment: 0,
+      interest: 10,
+    }
+  }
+
+  changeHandler = (e) => {
+    let name = e.currentTarget.name;
+    let value = e.currentTarget.value;
+    this.setState({
+      [name]: value
+    })
+  };
+
+  generateYears = () => {
+    let {
+      maxYears
+    } = this.state;
+    let options = [];
+    for (let i=1; i<=maxYears; i++) {
+      options.push(
+        <option key={i} value={i}>{i}</option>
+      );
+    }
+    return options;
+  };
+
+  repaymentCalc = (e) => {
+    e.preventDefault();
+    let {
+      years,
+      loan,
+      interest
+    } = this.state;
+    let convertedInt = (interest + 100)/100;
+    let payment = loan * Math.pow(convertedInt, years);
+    payment = payment.toFixed(2);
+    this.setState({
+      payment
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.repaymentCalc}>
+          <div>
+            <label htmlFor="interest">Interest (%)
+              <input name="interest" onChange={this.changeHandler} value={this.state.interest}/>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="loan">Amount
+              <input name="loan" onChange={this.changeHandler} value={this.state.loan} type="number"/>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="years">Years
+              <select onChange={this.changeHandler} name="years">
+                {this.generateYears()}
+              </select>
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.payment}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
